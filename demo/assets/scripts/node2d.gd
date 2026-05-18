683a0f89ed64a3489443e41e38385391cbaa8e0f
Denomycor <afonsoresteves@hotmail.com>
2026-05-18 14:37:57 +0100

    Testing stuff

--- demo/assets/scripts/node2d.gd
+++ demo/assets/scripts/node2d.gd
@@ -5,7 +5,7 @@ extends Node2D
 
 func _ready() -> void:
 	print("hello")
-	var weights := PackedFloat64Array([1,1,1])
+	var weights := PackedFloat64Array([1,1,1,1,1])
 	var wfc := WFCEngine2D.make_generator(Vector2i(20,20), weights, false, 0)
 	wfc.stepped.connect(func(_wfc: WFCEngine2D):
 		print("step")
@@ -14,10 +14,85 @@ func _ready() -> void:
 		print("finished")
 	)
 
-	wfc.change_constraint_rule(1, WFCEngine2D.Directions.UP, 2, false)
+	##--------- #0
+
+	wfc.change_constraint_rule(0, WFCEngine2D.Directions.DOWN, 1, false)
+	wfc.change_constraint_rule(0, WFCEngine2D.Directions.UP, 1, false)
+	wfc.change_constraint_rule(0, WFCEngine2D.Directions.LEFT, 1, false)
+	wfc.change_constraint_rule(0, WFCEngine2D.Directions.RIGHT, 1, false)
+	
+	wfc.change_constraint_rule(0, WFCEngine2D.Directions.UP, 2, false)
+	wfc.change_constraint_rule(0, WFCEngine2D.Directions.LEFT, 2, false)
+	wfc.change_constraint_rule(0, WFCEngine2D.Directions.RIGHT, 2, false)
+
+	wfc.change_constraint_rule(0, WFCEngine2D.Directions.DOWN, 3, false)
+	wfc.change_constraint_rule(0, WFCEngine2D.Directions.UP, 3, false)
+	wfc.change_constraint_rule(0, WFCEngine2D.Directions.LEFT, 3, false)
+	wfc.change_constraint_rule(0, WFCEngine2D.Directions.RIGHT, 3, false)
+
+	wfc.change_constraint_rule(0, WFCEngine2D.Directions.DOWN, 2, false)
+	wfc.change_constraint_rule(0, WFCEngine2D.Directions.LEFT, 2, false)
+	
+	##--------- #1
+
 	wfc.change_constraint_rule(1, WFCEngine2D.Directions.DOWN, 2, false)
 	wfc.change_constraint_rule(1, WFCEngine2D.Directions.LEFT, 2, false)
 	wfc.change_constraint_rule(1, WFCEngine2D.Directions.RIGHT, 2, false)
+
+	wfc.change_constraint_rule(1, WFCEngine2D.Directions.DOWN, 3, false)
+	wfc.change_constraint_rule(1, WFCEngine2D.Directions.LEFT, 3, false)
+
+	wfc.change_constraint_rule(1, WFCEngine2D.Directions.DOWN, 4, false)
+	wfc.change_constraint_rule(1, WFCEngine2D.Directions.UP, 4, false)
+	wfc.change_constraint_rule(1, WFCEngine2D.Directions.LEFT, 4, false)
+	wfc.change_constraint_rule(1, WFCEngine2D.Directions.RIGHT, 4, false)
+
+	##-------- #2
+
+	wfc.change_constraint_rule(2, WFCEngine2D.Directions.DOWN, 2, false)
+	wfc.change_constraint_rule(2, WFCEngine2D.Directions.UP, 2, false)
+
+	wfc.change_constraint_rule(2, WFCEngine2D.Directions.DOWN, 3, false)
+	wfc.change_constraint_rule(2, WFCEngine2D.Directions.RIGHT, 3, false)
+	wfc.change_constraint_rule(2, WFCEngine2D.Directions.UP, 3, false)
+
+	wfc.change_constraint_rule(2, WFCEngine2D.Directions.DOWN, 4, false)
+	wfc.change_constraint_rule(2, WFCEngine2D.Directions.RIGHT, 4, false)
+	wfc.change_constraint_rule(2, WFCEngine2D.Directions.LEFT, 4, false)
+
+	##-------- #3
+
+	wfc.change_constraint_rule(3, WFCEngine2D.Directions.DOWN, 3, false)
+	wfc.change_constraint_rule(3, WFCEngine2D.Directions.UP, 3, false)
+	wfc.change_constraint_rule(3, WFCEngine2D.Directions.RIGHT, 3, false)
+	wfc.change_constraint_rule(3, WFCEngine2D.Directions.LEFT, 3, false)
+
+	wfc.change_constraint_rule(3, WFCEngine2D.Directions.DOWN, 4, false)
+	wfc.change_constraint_rule(3, WFCEngine2D.Directions.UP, 4, false)
+	wfc.change_constraint_rule(3, WFCEngine2D.Directions.RIGHT, 4, false)
+	wfc.change_constraint_rule(3, WFCEngine2D.Directions.LEFT, 4, false)
+
+	##-------- #4
+
+	wfc.change_constraint_rule(4, WFCEngine2D.Directions.DOWN, 4, false)
+	wfc.change_constraint_rule(4, WFCEngine2D.Directions.UP, 4, false)
+	wfc.change_constraint_rule(4, WFCEngine2D.Directions.RIGHT, 4, false)
+	wfc.change_constraint_rule(4, WFCEngine2D.Directions.LEFT, 4, false)
+	
+
+	##------- #Variants
+
+	wfc.generate_variant_rule(2, WFCEngine2D.Variants.ROT90)
+	wfc.generate_variant_rule(2, WFCEngine2D.Variants.ROT180)
+	wfc.generate_variant_rule(2, WFCEngine2D.Variants.ROT270)
+
+	wfc.generate_variant_rule(3, WFCEngine2D.Variants.ROT90)
+	wfc.generate_variant_rule(3, WFCEngine2D.Variants.ROT180)
+	wfc.generate_variant_rule(3, WFCEngine2D.Variants.ROT270)
+
+	wfc.generate_variant_rule(4, WFCEngine2D.Variants.ROT90)
+	wfc.generate_variant_rule(4, WFCEngine2D.Variants.ROT180)
+	wfc.generate_variant_rule(4, WFCEngine2D.Variants.ROT270)
 
 	wfc.init()
 	var failed := wfc.run()
